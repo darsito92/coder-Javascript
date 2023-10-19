f7b49226b4a1a77bc4366810e944255d375fafbd
Diego Sepulveda <diego.sepulvedat92@gmail.com>
2023-10-19 20:58:56 -0300

    first commit

---
+++ clase-1/index.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(prompt("Ingrese un numero"));
+const num2 =3;
+const resultado= num1+num2;
+alert(resultado)
+
+const nombre = prompt("Ingrese su nombre:");
+const apellido = prompt("Ingrese su apellido");
+alert(nombre + " " + apellido);
