51e8e5874e28a94f7822ca24480295fdf7ba616c
Diego Sepulveda <diego.sepulvedat92@gmail.com>
2023-10-26 22:11:45 -0300

    clase 3

---
+++ clase-2/js/index.js
@@ -0,0 +1,51 @@
+// const num1 = parseInt(prompt("Ingrese un numero"));
+// const num2 =3;
+// const resultado= num1+num2;
+// // tipo de dato boleano
+// if(resultado>50){
+//     alert(resultado)
+// }
+
+// const miColorDePelo = "Negro";
+// const elColorDeOtraPersona = prompt("cual es tu color de pelo?");
+
+// if(elColorDeOtraPersona == miColorDePelo){
+//     alert("wow tenemos el mismo color de pelo");
+// }else{
+//     alert("tu pelo es de color "+ elColorDeOtraPersona);
+// }
+// ! NOT devuelve contrario boleano 
+// distinto 
+// !=
+// distinto que valida el tipo de dato
+// !==
+//boleano (true o false)
+// >,< >=, <=
+
+
+// const miEdad =parseInt(prompt("ingresa tu edad para saber eres mayor"));
+// const siEsMayor = miEdad>= 18;
+// console.log(siEsMayor);
+ 
+// and (y) -> &&
+//or (o) -> ||
+
+const num = parseInt(prompt("ungresa un numero"));
+if(num>1000){
+    alert("el numero es mayor que 1000");
+}
+
+const palabra = prompt("ungresa un saludo")
+if(palabra == "hola"){
+    console.log(palabra)
+} 
+
+const num2 =parseInt(prompt("ingresa un numero"))
+    if(num2>10&&num2<50){
+        alert(num2) 
+}
+
+
+
+
+
---
+++ clase-3/index.js
@@ -0,0 +1,6 @@
+let num =parseInt(prompt("ingresa un numero"));
+for(i=0;i<num;i++){
+    let sim = "#"
+    console.log(sim)
+
+}
