2d5688e8f660ef95af8fdb7def669b8a600c3067
Diego Sepulveda <diego.sepulvedat92@gmail.com>
2023-11-17 02:13:40 -0300

    clase 7

---
+++ clase-7/js/index.js
@@ -0,0 +1,11 @@
+console.log("clase-7");
+
+const funcion3 = (param1,param2)=>{
+const resultado =param1+param2
+return resultado
+}
+
+const resultadoFuncion3 = funcion3(1,2)
+console.log(resultadoFuncion3);
+
+
